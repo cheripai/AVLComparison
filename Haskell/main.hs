27dbb5dbd8dd31fa6d56ba9a5f2901c1f07099e8
Dat Do <dat@debian-GS70.home-domain>
2014-09-10 13:27:37 -0700

    Union runs in parallel

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -1,16 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 import Control.DeepSeq.TH
 import Control.Concurrent
-import Data.Maybe
 import Data.Tree.AVL as A
 import Data.COrdering
 import System.CPUTime
 import qualified Data.ByteString.Char8 as B
-import Control.Monad.Par
 import Control.DeepSeq
 import Data.List.Split
-import Control.Monad.IO.Class
 import System.Environment
+import Control.Parallel
 
 $(deriveNFData ''AVL)
 
@@ -23,21 +21,18 @@ main = do
     args <- getArgs
     contents <- fmap B.lines $ B.readFile $ args !! 0
     let l = splitEvery (length contents `div` 4) contents
-    deepseq contents $ deepseq l $ return  ()
+    deepseq contents $ deepseq l $ return ()
     start <- getCPUTime
-    runParIO $ do
-        t1 <- spawn (return $ load empty $ l !! 0)
-        t2 <- spawn (return $ load empty $ l !! 1)
-        t3 <- spawn (return $ load empty $ l !! 2)
-        t4 <- spawn (return $ load empty $ l !! 3)
-        t1' <- get t1
-        t2' <- get t2
-        t3' <- get t3
-        t4' <- get t4
-        let b' = union fstCC t1' t2'
-        let t' = union fstCC t3' t4'
-        let bt = union fstCC b' t'
-        return ()
+    let t1 = load empty $ l !! 0
+    let t2 = load empty $ l !! 1
+    let t3 = load empty $ l !! 2
+    let t4 = load empty $ l !! 3
+    let p = par t1 $ par t2 $ par t3 t4
+    let b = union fstCC t1 t2
+    let t = union fstCC t3 t4
+    let bt = union fstCC b t
+    let bt' = par b $ par t bt
+    deepseq p $ deepseq bt' $ return ()
     end <- getCPUTime
     n <- getNumCapabilities
     let diff = ((fromIntegral (end-start)) / (10^12) / fromIntegral n)
